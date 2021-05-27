9ce2a6606cf52887a29c46b65f8a0ebaa22557db
DV <mumunchik2001@gmail.com>
2021-05-27 19:46:20 +0300

    adding next

--- homework/lesson1/tour1.go
+++ homework/lesson1/tour1.go
@@ -6,8 +6,19 @@ import (
 	"math/rand"
 )
 
+func add(x int, y int) int {
+	return x + y
+}
+
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
 func main() {
 	fmt.Println("random number is", rand.Intn(100))
-
 	fmt.Printf("Now you have %g problems. \n", math.Sqrt(100))
+	fmt.Println(math.Pi)
+	fmt.Println(add(17, 43))
+	a, b := swap("Hello", "World")
+	fmt.Println(a, b)
 }
