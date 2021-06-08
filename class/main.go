8374bb324a6fafda704f2527508b432688d45447
Alexander Gatsiha <micro1985@mail.ru>
2021-06-08 09:37:26 +0300

    CLI app done, I hope so

--- class/main.go
+++ class/main.go
@@ -1,20 +1,20 @@
 package main
 
-import ( 
-	"fmt"
-	"net/http"
-	"io"
+import (
 	"encoding/json"
 	"flag"
+	"fmt"
+	"io"
+	"net/http"
 )
 
 const (
 	basic_url = "http://api.weatherapi.com/v1/current.json?key=34976b7e974d4c67beb163221210106&q="
 )
 
-type RequestData struct{
+type RequestData struct {
 	city string
-	air string
+	air  string
 }
 
 type GotData struct {
@@ -56,6 +56,16 @@ type GotData struct {
 		Uv         float64 `json:"uv"`
 		GustMph    float64 `json:"gust_mph"`
 		GustKph    float64 `json:"gust_kph"`
+		AirQuality struct {
+			Co           float64 `json:"co"`
+			No2          float64 `json:"no2"`
+			O3           float64 `json:"o3"`
+			So2          float64 `json:"so2"`
+			Pm25         float64 `json:"pm2_5"`
+			Pm10         float64 `json:"pm10"`
+			UsEpaIndex   int     `json:"us-epa-index"`
+			GbDefraIndex int     `json:"gb-defra-index"`
+		} `json:"air_quality"`
 	} `json:"current"`
 }
 
@@ -64,35 +74,44 @@ func build_url(r RequestData) string {
 	return res
 }
 
-func parse_request(str string) {
+func parse_request(str string, air string) {
 	data := GotData{}
-    json.Unmarshal([]byte(str), &data)
-    fmt.Printf("Country: %s", data.Location.Country)
+	json.Unmarshal([]byte(str), &data)
+	fmt.Println("\n")
+	fmt.Printf("Country: %s", data.Location.Country)
 	fmt.Println("\n")
 	fmt.Printf("Name: %s", data.Location.Name)
 	fmt.Println("\n")
 	fmt.Printf("Temperature: %f", data.Current.TempC)
-} 
+	fmt.Println("\n")
+	if air == "yes" {
+		fmt.Printf("Air quality Co: %f \n", data.Current.AirQuality.Co)
+		fmt.Printf("Air quality No2: %f \n", data.Current.AirQuality.No2)
+		fmt.Printf("Air quality O3: %f \n", data.Current.AirQuality.O3)
+		fmt.Printf("Air quality So2: %f \n", data.Current.AirQuality.So2)
+		fmt.Printf("Air quality Pm25: %f \n", data.Current.AirQuality.Pm25)
+		fmt.Printf("Air quality Pm10: %f \n", data.Current.AirQuality.Pm10)
+	}
+}
 
 func main() {
-	fmt.Println("HELLO!")
 
 	var r RequestData
-//	r.city = "Minsk"
-	r.air = "no"
+	// r.city = "Minsk"
+	// r.air = "yes"
 
 	a := flag.String("city", "Minsk", "Enter City name for weather")
+	b := flag.String("air", "no", "Enter air quality necessity")
 	flag.Parse()
 	r.city = *a
-	fmt.Println(r.city)
-	
+	r.air = *b
+
 	res := build_url(r)
 	content, _ := http.Get(res)
 	defer content.Body.Close()
 	body, _ := io.ReadAll(content.Body)
 	bodystr := string(body)
-	//fmt.Println(bodystr)
 
-	parse_request(bodystr)
+	parse_request(bodystr, r.air)
 	fmt.Println("\n")
 }
